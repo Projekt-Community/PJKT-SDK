04a8bb57d5f6239857323b08d16f440741670bbb
Karet <5xa1xa@gmail.com>
2024-05-23 18:17:04 -0500

    fixed user profile not loading on login/register and booth descriptor bounds not being set

--- Packages/com.pjkt.sdk/Editor/Visual Elements/EventButton.cs
+++ Packages/com.pjkt.sdk/Editor/Visual Elements/EventButton.cs
@@ -42,6 +42,7 @@ namespace PJKT.SDK2
         {
             //assess the booth
             PjktEventManager.SelectedProjekt = Projekt;
+            BoothDescriptor._maxBounds = new Vector3(requirements.MaxDims[0], requirements.MaxDims[2], requirements.MaxDims[1]);
             BoothValidator.Requirements = requirements;
             BoothValidator.GenerateReport();
             
