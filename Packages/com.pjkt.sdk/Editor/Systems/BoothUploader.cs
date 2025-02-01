f88f929511bd4c932f9c8da9a5cfc5b306a45d57
Karet <5xa1xa@gmail.com>
2025-02-01 16:27:18 -0600

    removed old code, seperate comit in case i need it

--- Packages/com.pjkt.sdk/Editor/Systems/BoothUploader.cs
+++ Packages/com.pjkt.sdk/Editor/Systems/BoothUploader.cs
@@ -62,56 +62,6 @@ namespace PJKT.SDK2
             if (success) PjktSdkWindow.Notify("Booth Uploaded Successfully. See you at the event!");
         }
 
-        /*
-        private static async Task<bool> CreateBoothPackage(BoothDescriptor boothDescriptor)
-        {
-            prefabPath = defaultPrefabPath + boothDescriptor.currentCommunity +"/" + boothDescriptor.boothName + ".prefab";
-            packagePath = defaultPackagePath + boothDescriptor.currentCommunity +"/" + boothDescriptor.boothName + ".unitypackage";
-
-            boothDescriptor.SDKVersion = "2.1.3";
-            
-            try
-            {
-                //Turn the booth into a prefab
-                CreateDirectory(prefabPath);
-                
-                // Clone this object to avoid modifying the original
-                var boothDescriptorNew = UnityEngine.Object.Instantiate(boothDescriptor.gameObject);
-                
-                GameObject prefab = PrefabUtility.SaveAsPrefabAsset(boothDescriptorNew, prefabPath);
-                AssetDatabase.ImportAsset(prefabPath);
-            
-                //Wait for this file to exist
-                while (!File.Exists(prefabPath)) { await Task.Delay(100); }
-
-                //TODO: Strip components from the prefab, including Bakery components and unoptimized Poiyomi shaders
-                //Really anything that will cause unnecessary bloat
-                //Perhaps all but whitelisted components?
-
-                //Export the UnityPackage
-                CreateDirectory(packagePath);
-
-                //Assemble the export options
-                ExportPackageOptions packageOptions = ExportPackageOptions.Default;
-                packageOptions |= ExportPackageOptions.Recurse;
-                packageOptions |= ExportPackageOptions.IncludeDependencies;
-
-                AssetDatabase.ExportPackage(new string[] { prefabPath }, packagePath, packageOptions);
-
-                //Wait for this file to exist
-                while (!File.Exists(packagePath)) { await Task.Delay(100); }
-                
-                boothDescriptorNew.name = boothDescriptor.boothName + " (Uploaded Copy, for reference only)";
-
-                return true;
-            }
-            catch (Exception e)
-            {
-                PjktSdkWindow.Notify($"Booth upload failed, Couldn't create booth package\n {e}", BoothErrorType.Error);
-                return false;
-            }
-        }*/
-
         private static async Task<bool> UploadBoothToServer(BoothDescriptor booth)
         {
             try
@@ -151,7 +101,6 @@ namespace PJKT.SDK2
                 return false;
             }
         }
-
         
         private static void CleanupOperations(BoothDescriptor booth)
         {
@@ -160,38 +109,6 @@ namespace PJKT.SDK2
             if (!File.Exists(packagePath)) return;
             if (Path.GetExtension(packagePath) != ".zip") return; //dont delete random stuff pls
             File.Delete(packagePath);
-
-            /*
-            //Delete the temporary files and folders
-            if (File.Exists(prefabPath)) File.Delete(prefabPath);
-            string prefabFolderPath = prefabPath.TrimEnd('.','p','r','e','f','a','b');
-            if (File.Exists(prefabFolderPath + ".meta")) File.Delete(prefabFolderPath + ".meta");
-            
-            if (File.Exists(packagePath)) File.Delete(packagePath);
-            //string packagefolderPath = packagePath.TrimEnd('.','u','n','i','t','y','p', 'a', 'c', 'k', 'a', 'g', 'e');
-            //if (File.Exists(packagefolderPath + ".meta")) File.Delete(packagefolderPath + ".meta");
-            
-            if (UnityEngine.Windows.Directory.Exists(Path.GetDirectoryName(prefabFolderPath))) Directory.Delete(Path.GetDirectoryName(prefabFolderPath));
-            if (UnityEngine.Windows.Directory.Exists(Path.GetDirectoryName(prefabFolderPath)  + ".meta")) Directory.Delete(Path.GetDirectoryName(prefabFolderPath) + ".meta");
-            if (UnityEngine.Windows.File.Exists(defaultPrefabPath + booth.currentCommunity +".meta")) File.Delete(defaultPrefabPath + booth.currentCommunity +".meta");
-
-            prefabPath = "";
-            packagePath = "";
-            
-            //Refresh the asset database
-            AssetDatabase.Refresh();*/
         }
-        /*
-        public static void CreatePath(string file)
-        {
-            if (Directory.Exists(file)) return;
-            CreatePath(Path.GetDirectoryName(file));
-            Directory.CreateDirectory(file);
-        }
-
-        public static void CreateDirectory(string file)
-        {
-            CreatePath(Path.GetDirectoryName(file));
-        }*/
     }
 }
