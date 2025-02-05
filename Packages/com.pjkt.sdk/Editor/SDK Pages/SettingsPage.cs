1773d0e501efcdf431b465db36fe0e897776836a
Karet <5xa1xa@gmail.com>
2025-02-05 00:42:21 -0600

    2025 prefabs

--- Packages/com.pjkt.sdk/Editor/SDK Pages/SettingsPage.cs
+++ Packages/com.pjkt.sdk/Editor/SDK Pages/SettingsPage.cs
@@ -20,9 +20,13 @@ namespace PJKT.SDK2
             ExamplePrefab booth2024 = new ExamplePrefab("PJKT2024 Prefab Booth", "A simple booth used in the 2024 Projekt:Fest event", "Assets/PJKT/Booth 2024/Booth Prefab.prefab", "Packages/com.pjkt.sdk/Runtime/Examples/PJKT2024PrefabBooth.unitypackage");
             scrollView.Add(booth2024);
             
-            //2024 button prefabs
-            ExamplePrefab button2024 = new ExamplePrefab("PJKT2024 Button Prefabs", "A collection of buttons for toggling objects and animations", "Assets/PJKT/Button Prefabs 2024/Toggle Button.prefab", "Packages/com.pjkt.sdk/Runtime/Examples/PJKT2024ButtonPrefabs.unitypackage");
-            scrollView.Add(button2024);
+            //2024 booth prefab
+            ExamplePrefab booth2025 = new ExamplePrefab("PJKT2025 Prefab Booth", "A simple booth used in the 2025 Projekt:Fest event", "Assets/PJKT/Prefab Booth 2025/PJKT Booth 2025 Prefab.prefab", "Packages/com.pjkt.sdk/Runtime/Examples/PJKT Booth 2025.unitypackage");
+            scrollView.Add(booth2025);
+            
+            //2025button prefabs
+            ExamplePrefab button2025 = new ExamplePrefab("PJKT2025 Button Prefabs", "A collection of buttons for toggling objects and animations", "Assets/PJKT/Button Prefabs 2025/Button Prefabs/Object Toggles/PJKT Toggle Button.prefab", "Packages/com.pjkt.sdk/Runtime/Examples/PJKT Button Prefabs 2025.unitypackage");
+            scrollView.Add(button2025);
         }
     }
 }
