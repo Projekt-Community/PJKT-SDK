c01df28eb8466e45bf13056e204af7a5ffbb2a90
Karet <5xa1xa@gmail.com>
2026-03-11 17:09:19 -0500

    added gizmo for booth orentation

--- Packages/com.pjkt.sdk/Runtime/BoothDescriptor.cs
+++ Packages/com.pjkt.sdk/Runtime/BoothDescriptor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEditor;
 using UnityEngine;
 
 namespace PJKT.SDK2
@@ -19,7 +20,8 @@ namespace PJKT.SDK2
         
         
         #if !COMPILER_UDONSHARP && UNITY_EDITOR
-            private void OnDrawGizmosSelected() {
+            private void OnDrawGizmosSelected() 
+            {
                 //----------Draw a box around the booth----------//
                     //Get all renderers
                 Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer>();
@@ -64,6 +66,28 @@ namespace PJKT.SDK2
                 }
             }
         
+        private void OnDrawGizmos() 
+        {
+            Matrix4x4 previousGizmosMatrix = Gizmos.matrix;
+            Matrix4x4 previousHandlesMatrix = Handles.matrix;
+
+            Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+            Handles.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+            
+            Gizmos.color = new Color(0.2f, 0.2f, 0.8f, 0.5f);
+            
+            Gizmos.DrawWireCube(Vector3.zero, new Vector3(_maxBounds.x, 0.01f, _maxBounds.z));
+            Handles.Label(Vector3.zero + new Vector3(_maxBounds.x / 2 + .2f, 0, 0), "Floor Plane", EditorStyles.boldLabel);
+            
+            Gizmos.color = new Color(0.4f, 0.2f, 0.8f, 0.5f);
+            Vector3 indicatorPos = new Vector3(0, 0, _maxBounds.z / 2);
+
+            Gizmos.DrawMesh(Resources.GetBuiltinResource<Mesh>("Cylinder.fbx"), indicatorPos, Quaternion.identity, new  Vector3(.3f, .01f, .3f));
+            Handles.Label(indicatorPos + new Vector3(0, .1f, 0), "Front", EditorStyles.boldLabel);
+
+            Gizmos.matrix = previousGizmosMatrix;
+            Handles.matrix = previousHandlesMatrix;
+        }
 #endif
     }
 }
